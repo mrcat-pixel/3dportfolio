75c582311ebeb95950b043f42f32640fe9093b94
mrcat-pixel <mrcat.pixel@gmail.com>
2022-05-10 21:26:43 +0300

    Text drawing, cursor, side panel

---
+++ script/2dui.js
@@ -0,0 +1,49 @@
+let hover_button = -1;
+let active_button = -1;
+let positions = new Array(6);
+for (let i = 0; i < 7; i++) positions[i] = 0;
+
+const cursor = curDot({
+    zIndex: 200,
+    diameter: 50,
+    borderWidth: 0,
+    easing: 2,
+    background: '#fff'
+});
+
+function setHoverButton(num) { hover_button = num; }
+function setActiveButton(num) {
+    active_button = active_button === num? -1 : num;
+    for (let i = 0; i < 7; i++) {
+        document.getElementById('bt' + i).style.color = i === active_button? 'red' : 'white';
+    }
+}
+
+function animate_filter_buttons() {
+    for (let i = 0; i < 7; i++) {
+        let target_pos = hover_button === i ? 20 : 0;
+        positions[i] += (target_pos - positions[i]) / 7;
+    }
+    for (let i = 0; i < 7; i++) {
+        document.getElementById('bt' + i).style.paddingLeft = positions[i] + 'px';
+    }
+}
+
+document.getElementById('bt6').addEventListener('click', () => {
+    document.getElementById('viewport-contain').style.filter = "blur(20px)";
+})
+
+function animate() {
+    requestAnimationFrame(animate);
+    animate_filter_buttons();
+    animate_hamburger_menu_buttons();
+}
+
+for (let i = 0; i < 7; i++) {
+    let element = document.getElementById('bt' + i);
+    element.addEventListener('mouseenter', () => setHoverButton(i));
+    element.addEventListener('mouseleave', () => setHoverButton(-1));
+    element.addEventListener('click', () => setActiveButton(i));
+}
+
+animate();
--- script/3dspace.js
+++ script/3dspace.js
@@ -2,28 +2,79 @@
 
 const renderer = new THREE.WebGLRenderer({antialias: true});
 renderer.setSize(window.innerWidth, window.innerHeight);
-renderer.setClearColor(0x999999, 1);
+renderer.setClearColor(0x000000, 1);
 const renderer_element = renderer.domElement;
 document.getElementById("viewport-contain").appendChild(renderer_element);
 
 const scene = new THREE.Scene();
 
 let camera = new THREE.PerspectiveCamera(70, window.innerWidth/window.innerHeight);
-camera.position.set(0, 0, 25);
+camera.position.set(0, 0, 50);
 let camera_target = new THREE.Vector3(0, 0, 50);
 scene.add(camera);
 
-const boxGeometry = new THREE.BoxGeometry(12.5, 20, 2);
-const basicMaterial = new THREE.MeshBasicMaterial({color: 0xffffff});
-const cube1 = new THREE.Mesh(boxGeometry, basicMaterial);
-cube1.position.set(0, 0, 0);
-scene.add(cube1);
-const cube2 = new THREE.Mesh(boxGeometry, basicMaterial);
-cube2.position.set(15, 10, 0);
-scene.add(cube2);
-const cube3 = new THREE.Mesh(boxGeometry, basicMaterial);
-cube3.position.set(-15, -10, 0);
-scene.add(cube3);
+class Pane {
+    constructor(scene, title, desc, x, y) {
+        const canvas = document.createElement( "canvas" );
+        let height = 800;
+        let width = 0.75*height;
+
+        canvas.width = width;
+        canvas.height = height;
+
+        var ctx = canvas.getContext("2d");
+        ctx.fillStyle = "white";
+        ctx.fillRect(0, 0, width, height);
+
+        ctx.fillStyle = "black";
+        ctx.font = 0.128 * height + "px texfont";
+        ctx.fillText(title, 0.066*width, 0.75*height);
+
+        ctx.font = 0.032 * height + "px texfont";
+        for ( let i = 0; i < desc.length; i ++ )
+            ctx.fillText( desc[ i ], 0.066*width, 0.04 * height * i + 0.85*height );
+
+        let texture = new THREE.CanvasTexture(canvas);
+
+        this.backpane = new THREE.Mesh(
+            new THREE.PlaneGeometry(15, 20),
+            new THREE.MeshBasicMaterial({map: texture})
+        );
+        this.backpane.position.set(x, y, 0);
+        scene.add(this.backpane);
+    }
+}
+
+let pane1 = new Pane(scene,
+    'math4',
+    ['Function interpolation via', 'the Lagrange polynomial in Python.'],
+    0, 0
+);
+
+let pane2 = new Pane(scene,
+    'duo',
+    ['perduo', 'perduo', 'perduo'],
+    17, 0
+);
+
+let pane3 = new Pane(scene,
+    'duo',
+    ['perduo', 'perduo', 'perduo'],
+    34, 0
+);
+
+let pane4 = new Pane(scene,
+    'duo',
+    ['perduo', 'perduo', 'perduo'],
+    -17, 0
+);
+
+let pane5 = new Pane(scene,
+    'duo',
+    ['perduo', 'perduo', 'perduo'],
+    -34, 0
+);
+
 
 // code
 
@@ -40,7 +91,7 @@ renderer_element.addEventListener('wheel', (evt) => {
 let mouse_position_on_click = {};
 let is_mouse_down = false;
 renderer_element.addEventListener('mousedown', () => { is_mouse_down = true; });
-renderer_element.addEventListener('mouseup', () => { is_mouse_down = false; });
+document.addEventListener('mouseup', () => { is_mouse_down = false; });
 renderer_element.addEventListener('mousemove', (evt) => {
     if (typeof(mouse_position_on_click.x) != 'undefined' && is_mouse_down) {
         let pan_spd = 0.0014 * camera_target.z;
@@ -70,7 +121,9 @@ const clamp = (num, min, max) => Math.min(Math.max(num, min), max);
 function clampCamera() {
     camera_target.x = clamp(camera_target.x, -50, 50);
     camera_target.y = clamp(camera_target.y, -50, 50);
-    camera_target.z = clamp(camera_target.z, 20, 75);
+    camera_target.z = clamp(camera_target.z, 15, 75);
+
+    camera.rotation.y = clamp(camera.rotation.y, 0, 45);
 }
 
 function render() {
